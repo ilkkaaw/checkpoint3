b5139c5adacef8c50a764097aeb651b34a6aa5ef
ilkka.aw <ilkka.pekkala@awacademyconsultant.fi>
2021-07-09 09:12:59 +0300

    First step working

--- vko3-1/tehtava1.py
+++ vko3-1/tehtava1.py
@@ -1,1 +1,13 @@
-#TODO all.
+import requests
+
+data_to_be_read = requests.get("https://2ri98gd9i4.execute-api.us-east-1.amazonaws.com/dev/academy-checkpoint2-json")
+json_data = data_to_be_read.json()
+data_items = json_data["items"]
+
+try:
+    file_to_be_opened = open("checkpoint.txt", "w")
+    for x in data_items:
+        file_to_be_opened.write(x["parameter"] + "\n")
+    file_to_be_opened.close()
+except:
+    print("Something went wrong!")
